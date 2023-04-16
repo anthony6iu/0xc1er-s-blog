48b532bb43fc84c86b3e07312ca12797ec4da060
anthony6iu <anthony6iu@gmail.com>
2023-04-16 23:33:50 +0800

    update site title and desc

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,11 +14,11 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "0xCier",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "0xCier Web3 Inspiration.",
 	// Meta property used as a default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "0xCier的Web3个人博客",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
